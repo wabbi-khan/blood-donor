a525a13eb2044c4285829f42e6e2815d5075bdab
wahablion <wahab@blacklionapp.xyz>
2026-06-26 19:29:06 +0500

    ok

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
     }`;
 
   return (
-    <nav className="sticky top-0 z-50 glass-dark border-b border-white/10">
+    <nav className="sticky top-0 z-50 glass-dark border-b border-white/10 m-2.5">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
